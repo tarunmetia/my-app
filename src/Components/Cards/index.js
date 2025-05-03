24288bcd6b42e04cf21d67148d976512a72cc2f7
tarun <tarun.metia@gmail.com>
2025-05-03 19:27:36 +0530

    cart change

---
+++ src/Components/Cards/index.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect, Fragment} from 'react'
+import './cards.css'
+
+function Cards() {
+const [cardData, setCardData] = useState([])
+useEffect (() => {
+    const apiUrl = 'https://dummyjson.com/carts'
+    const getDataFromApi = async() => {
+        try {
+            const responseData = await fetch(apiUrl)
+            const jsondata = await responseData.json()
+            setCardData(jsondata.carts)
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+    getDataFromApi()
+}, [])
+
+  return (
+    <div >
+      {cardData.map ((item, index) => {
+        return (
+            <div className='cards-wrapper'>                
+                {item.products.map((productsItem, productIndex) => {
+                    return(
+                        <div className='card-Item'>
+                            <h2>{productsItem.title}</h2>
+                            <div className='image-wrapper'>
+                                <img src={productsItem.thumbnail} alt=""/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+      })}
+
+    </div>
+  )
+}
+
+export default Cards
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cards from '../../Components/Cards'
+function Home() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  )
+}
+
+export default Home
